34b55b97a780787e0a9fc6a697dc1b4365e94770
fereste <fernando.piubel@gmail.com>
2022-04-15 15:34:25 -0300

    Check cuando se copia

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function copyToClipboard(what) {
-    navigator.clipboard.writeText(document.querySelector(`[data-${what}]`).innerHTML);
+    // navigator.clipboard.writeText(document.querySelector(`[data-${what}]`).innerHTML);
+
+    document.querySelector('#copy').classList.add('hide');
+    document.querySelector('#tick').classList.remove('hide');
+    setTimeout(() => {
+        document.querySelector('#copy').classList.remove('hide');
+        document.querySelector('#tick').classList.add('hide');
+    }, 1000);
 }
